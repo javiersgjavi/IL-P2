e7a0bbe6d2672a93efca95ad3b2583b564d93ef5
Javier <javiersgjavi@gmail.com>
2021-11-27 17:49:35 +0100

    Añadido el contador de palabras

--- clean_text.py
+++ clean_text.py
@@ -11,20 +11,18 @@ def read_text(path):
 
     return words
 
-def count_words_sorted(words):
-    count_words = dict()
+def count_words(words, count_words):
     for word in words:
         if word in count_words:
             count_words[word] += 1
         else:
             count_words[word] = 1
 
-    sorted_count_words = dict(sorted(count_words.items(), key=lambda x: x[1], reverse=True))
-    return sorted_count_words
+    return count_words
 
 def delete_excluded_words(path, sorted_count_words):
     with open(path, 'r') as f:
-        excluded_words = f.read().split(',')
+        excluded_words = f.read().split('\n')
         excluded_words = [word.strip() for word in excluded_words]
 
         for excluded in excluded_words:
@@ -32,21 +30,32 @@ def delete_excluded_words(path, sorted_count_words):
 
     return sorted_count_words
 
-def write_output(sorted_count_words):
-    with open('output.txt', 'w') as f:
+def write_output(count_words, category):
+    sorted_count_words = dict(sorted(count_words.items(), key=lambda x: x[1], reverse=True))
+
+    if not os.path.exists('./outputs/'):
+        os.makedirs('./outputs/')
+
+    with open(f'./outputs/{category}.txt', 'w') as f:
         for word in sorted_count_words.keys():
             f.write(f'{word},{sorted_count_words[word]}\n')
 
-def main(path, excluded=None):
+def main(path_data, excluded=None):
+    for category in os.listdir(path_data):
+        res = dict()
+        path_category = f'./noticias/{category}/'
+        files = os.listdir(path_category)
+        
+        for file in files:
+            path_file = f'{path_category}{file}'
+            words = read_text(path_file)
+            res = count_words(words, res)
 
-    words = read_text(path)
-    sorted_count_words = count_words_sorted(words)
+            if excluded:
+                res = delete_excluded_words(excluded, res)
 
-    if excluded:
-        sorted_count_words = delete_excluded_words(excluded, sorted_count_words)
-
-    write_output(sorted_count_words)
+            write_output(res, category)
         
 if __name__ == '__main__':
     # Get the current working directory
-    main('./texto.txt', excluded='./excluded_words.txt')
+    main('./noticias', excluded='./stop_words_2.txt')
