99ca338aa5e2349d981565efad5c34a71ab7c541
Lepticidio <miguel.andujar@live.u-tad.com>
2021-12-03 19:22:27 +0100

    predicted category is calculated, overall and category accuracy printed

--- scripts/classifier.py
+++ scripts/classifier.py
@@ -67,30 +67,26 @@ def get_category_vector(path, glossary):
             else:
                 vector_values.append(0)
 
-        vector_category = np.array(vector_values)
-        print('vector category '+path+': ' + str(vector_category))
+        vector_category = np.array(vector_values, dtype=float)
         return vector_category
 
 def main(path_data, excluded=None, glossary_path=None, category_glossaries=None):
-
-
-
     glossary = read_text(glossary_path)
     glossary_dictionary = dict()
     glossary_dictionary = get_glossary_dictionary(glossary, glossary_dictionary)
 
-
-    csvs = os.listdir(category_glossaries)
-
-    category_vector_dictionary = dict()
-    for category_csv in csvs:
-        category_vector_dictionary[category_csv] = get_category_vector(f'{category_glossaries}{category_csv}', glossary_dictionary)
+    categories = []
+    category_vectors =[]
+    file_vectors = []
+    real_categories = []
 
     for category in os.listdir(path_data):
+        categories.append(category)
         path_category = f'./{path_data}/{category}/test/'
         files = os.listdir(path_category)
 
         for file in files:
+            real_categories.append(category)
             path_file = f'{path_category}{file}'
             words = read_text(path_file)
             res = glossary_dictionary.copy()
@@ -98,8 +94,48 @@ def main(path_data, excluded=None, glossary_path=None, category_glossaries=None)
             file_values = []
             for word in res:
                 file_values.append(res[word])
-            vector_file = np.array(file_values)
+            vector_file = np.array(file_values, dtype=float)
+            file_vectors.append(vector_file)
+
+            print('category: '+category+' vector: ' + str(vector_file))
             write_output(res, category+'/'+file)
+
+    csvs = os.listdir(category_glossaries)
+    category_vector_dictionary = dict()
+    for category_csv in csvs:
+        category_vectors.append(get_category_vector(f'{category_glossaries}{category_csv}', glossary_dictionary))
+
+    counter = 0
+    right_guesses_counter = 0
+    category_right_guesses = dict()
+    for vector in file_vectors:
+        category_counter = 0
+        max_value = 0
+        predicted_category = ''
+        for category_vector in category_vectors:
+            product = np.dot(vector, category_vector)
+            similarity = product / (np.linalg.norm(vector)*np.linalg.norm(category_vector))
+            if similarity >= max_value:
+                max_value = similarity
+                predicted_category = categories[category_counter]
+            #print('file'+str(counter)+ ' '+ str(categories[category_counter])+ ': ' + str(similarity))
+            category_counter += 1
+        real_category = real_categories[counter]
+        print('file'+str(counter)+ ' predicted_category: '+ predicted_category + ' real_category: ' + real_category)
+        if predicted_category == real_category:
+            right_guesses_counter += 1
+            if real_category in category_right_guesses:
+                category_right_guesses[real_category] += 1
+            else:
+                category_right_guesses[real_category] = 1
+        counter += 1
+    total_size = len(file_vectors)
+    accuracy = right_guesses_counter / total_size
+    print('overall accuracy: ' + str(accuracy))
+    for category in category_right_guesses:
+        print(category + " accuracy: " + str(category_right_guesses[category]/(total_size/3)))
+
+
 
 if __name__ == '__main__':
     # Get the current working directory
