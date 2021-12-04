3c651bc2d03024e87c9d16d6e76533a71690394e
Lepticidio <miguel.andujar@live.u-tad.com>
2021-12-04 11:14:56 +0100

    Tried with other glossary sizes, 50 is better

--- scripts/tf-idf_calculator.py
+++ scripts/tf-idf_calculator.py
@@ -88,6 +88,36 @@ def get_glosaries(data, path):
 
         data_category.to_csv(f'{path_all}{category}.csv')
         data_category.iloc[:30].to_csv(f'{path_30}{category}.csv')
+def get_glosaries_40(data, path):
+
+    path_all = f'{path}/todos_40/'
+    path_40 = f'{path}/40/'
+    if not os.path.exists(path_all):
+        os.makedirs(path_all)
+
+    if not os.path.exists(path_40):
+        os.makedirs(path_40)
+
+    for category in data.columns[-3:]:
+        data_category = data[category].sort_values(ascending=False)
+
+        data_category.to_csv(f'{path_all}{category}.csv')
+        data_category.iloc[:40].to_csv(f'{path_40}{category}.csv')
+def get_glosaries_45(data, path):
+
+    path_all = f'{path}/todos_45/'
+    path_45 = f'{path}/45/'
+    if not os.path.exists(path_all):
+        os.makedirs(path_all)
+
+    if not os.path.exists(path_45):
+        os.makedirs(path_45)
+
+    for category in data.columns[-3:]:
+        data_category = data[category].sort_values(ascending=False)
+
+        data_category.to_csv(f'{path_all}{category}.csv')
+        data_category.iloc[:45].to_csv(f'{path_45}{category}.csv')
 def get_glosaries_50(data, path):
 
     path_all = f'{path}/todos_50/'
@@ -103,6 +133,51 @@ def get_glosaries_50(data, path):
 
         data_category.to_csv(f'{path_all}{category}.csv')
         data_category.iloc[:50].to_csv(f'{path_50}{category}.csv')
+def get_glosaries_60(data, path):
+
+    path_all = f'{path}/todos_60/'
+    path_60 = f'{path}/60/'
+    if not os.path.exists(path_all):
+        os.makedirs(path_all)
+
+    if not os.path.exists(path_60):
+        os.makedirs(path_60)
+
+    for category in data.columns[-3:]:
+        data_category = data[category].sort_values(ascending=False)
+
+        data_category.to_csv(f'{path_all}{category}.csv')
+        data_category.iloc[:60].to_csv(f'{path_60}{category}.csv')
+def get_glosaries_75(data, path):
+
+    path_all = f'{path}/todos_75/'
+    path_75 = f'{path}/75/'
+    if not os.path.exists(path_all):
+        os.makedirs(path_all)
+
+    if not os.path.exists(path_75):
+        os.makedirs(path_75)
+
+    for category in data.columns[-3:]:
+        data_category = data[category].sort_values(ascending=False)
+
+        data_category.to_csv(f'{path_all}{category}.csv')
+        data_category.iloc[:75].to_csv(f'{path_75}{category}.csv')
+def get_glosaries_100(data, path):
+
+    path_all = f'{path}/todos_100/'
+    path_100 = f'{path}/100/'
+    if not os.path.exists(path_all):
+        os.makedirs(path_all)
+
+    if not os.path.exists(path_100):
+        os.makedirs(path_100)
+
+    for category in data.columns[-3:]:
+        data_category = data[category].sort_values(ascending=False)
+
+        data_category.to_csv(f'{path_all}{category}.csv')
+        data_category.iloc[:100].to_csv(f'{path_100}{category}.csv')
 def main(path):
     path_output = '../data/outputs/tf-idf/'
     if not os.path.exists(path_output):
